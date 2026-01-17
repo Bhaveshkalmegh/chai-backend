48bb0cbd8fb574105a6e282e148d4c4cfd9e82e6
Bhaveshkalmegh <bhaveshkalmegh9359@gmail.com>
2026-01-17 23:55:35 +0530

    DB connection Done

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// require('dotenv').config({path:"./.env"})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:"./.env"
+})
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express();
+
+(async ()=>{
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    
+    app.on("error",(error)=>{
+        console.log("error",error)
+        throw error;
+    })
+    app.listen(process.env.port,()=>{
+        console.log(`server running on http://localhost:${process.env.PORT}`);
+    })
+
+
+
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw error;
+  }  
+})()
+
+
+*/
