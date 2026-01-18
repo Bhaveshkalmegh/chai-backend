34427cf0250dc5f104230c35c4bb63bacaf3096c
Bhaveshkalmegh <bhaveshkalmegh9359@gmail.com>
2026-01-18 18:08:05 +0530

    create basic utilies

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,36 @@
+const asynchandler = (requesthandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requesthandler(req,res,next)).
+        catch((err)=>next(err))
+    }
+}   
+
+
+
+// using prosise .then catch block
+
+
+
+
+
+export {asynchandler};
+  
+    
+
+
+
+    // this is using try catch block 
+
+
+
+    // const asynchandler=(func)=>{async (req,res,next)=>{
+    //     try {
+    //         await func(req,res,next)
+    //     } catch (error) {
+    //         res.status(err.code||500).json({
+    //             success: false,
+    //             message: err.message
+                
+    //     })
+    //     }
+    // }}
